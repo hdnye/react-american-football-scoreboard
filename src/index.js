eb8d9e5ad309ec5d8cc932dbe2a4471437bfaecf
Heather Nye <heatherdnye@gmail.com>
2019-10-15 22:49:36 -0500

    mvp complete

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'; //{useState}// 
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -10,3 +10,25 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+//function App() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//}
